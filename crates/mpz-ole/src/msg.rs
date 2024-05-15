f367551850074753fa183ae58d133374da6c9124
th4s <th4s@metavoid.xyz>
2024-05-15 15:50:39 +0200

    Reworked message enum.

--- crates/mpz-ole/src/msg.rs
+++ crates/mpz-ole/src/msg.rs
@@ -2,40 +2,21 @@
 
 use enum_try_as_inner::EnumTryAsInner;
 use mpz_fields::Field;
+use mpz_ole_core::msg::{BatchAdjust, MaskedInputs};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, EnumTryAsInner, Serialize, Deserialize)]
+/// A message type for OLE.
+#[derive(Debug, EnumTryAsInner, Serialize, Deserialize)]
 #[derive_err(Debug)]
-/// A message type for ROLEe protocols.
-pub enum ROLEeMessage<F: Field> {
-    /// Random field elements sent by the provider.
-    ///
-    /// These are u_i and e_k.
-    RandomProviderMsg(Vec<F>, Vec<F>),
-    /// Random field elements sent by the evaluator.
-    ///
-    /// These are d_k.
-    RandomEvaluatorMsg(Vec<F>),
+pub enum OLEMessage<F: Field> {
+    /// Correlations sent by the sender to the receiver.
+    Masked(MaskedInputs<F>),
+    /// Adjustments sent to each other for share adjustment.
+    Adjust(BatchAdjust<F>),
 }
 
-impl<F: Field> From<ROLEeMessageError<F>> for std::io::Error {
-    fn from(err: ROLEeMessageError<F>) -> Self {
-        std::io::Error::new(std::io::ErrorKind::InvalidData, err.to_string())
-    }
-}
-
-#[derive(Debug, Clone, EnumTryAsInner, Serialize, Deserialize)]
-#[derive_err(Debug)]
-/// A message type for OLEe protocols.
-pub enum OLEeMessage<F: Field> {
-    /// Field elements sent by the provider.
-    ProviderDerand(Vec<F>),
-    /// Field elements sent by the evaluator.
-    EvaluatorDerand(Vec<F>),
-}
-
-impl<F: Field> From<OLEeMessageError<F>> for std::io::Error {
-    fn from(err: OLEeMessageError<F>) -> Self {
+impl<F: Field> From<OLEMessageError<F>> for std::io::Error {
+    fn from(err: OLEMessageError<F>) -> Self {
         std::io::Error::new(std::io::ErrorKind::InvalidData, err.to_string())
     }
 }
