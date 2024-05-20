d54b7e494c83629960c3432c3b059c62cd99b6d5
th4s <th4s@metavoid.xyz>
2024-05-20 10:10:04 +0200

    Return correct number of OLEs from `consume`.

--- crates/mpz-ole-core/src/receiver.rs
+++ crates/mpz-ole-core/src/receiver.rs
@@ -61,7 +61,7 @@ impl<const N: usize, F: Field> OLEReceiver<N, F> {
             return None;
         }
 
-        let shares = self.cache.split_off(count);
+        let shares = self.cache.split_off(self.cache.len() - count);
         Some(shares)
     }
 
