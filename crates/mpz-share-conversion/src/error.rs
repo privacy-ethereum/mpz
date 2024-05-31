8c90d0b67a5b3e906b9e68ee20e803f3507353f0
sinu <65924192+sinui0@users.noreply.github.com>
2024-05-31 11:21:30 -0700

    refactor(mpz-share-conversion): stub out refactored traits

--- crates/mpz-share-conversion/src/error.rs
+++ crates/mpz-share-conversion/src/error.rs
@@ -1,37 +1,19 @@
-use mpz_ot::OTError;
+use core::fmt;
 
-/// An error for what can go wrong during conversion
+/// A share conversion error.
 #[derive(Debug, thiserror::Error)]
-#[allow(missing_docs)]
-pub enum ShareConversionError {
-    #[error(transparent)]
-    OTError(Box<OTError>),
-    #[error(transparent)]
-    IOError(#[from] std::io::Error),
-    #[error("Received invalid seed")]
-    InvalidSeed,
-    #[error("Tape not configured")]
-    TapeNotConfigured,
-    #[error(transparent)]
-    TapeError(#[from] TapeVerificationError),
-    #[error("Already finalized")]
-    AlreadyFinalized,
+pub struct ShareConversionError {
+    #[allow(dead_code)]
+    kind: ErrorKind,
+    #[source]
+    source: Option<Box<dyn std::error::Error + Send + Sync>>,
 }
 
-impl From<mpz_ot::OTError> for ShareConversionError {
-    fn from(value: mpz_ot::OTError) -> Self {
-        ShareConversionError::OTError(Box::new(value))
+#[derive(Debug)]
+pub(crate) enum ErrorKind {}
+
+impl fmt::Display for ShareConversionError {
+    fn fmt(&self, _f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!()
     }
-}
-
-/// An error which can occur during tape verification
-#[derive(Debug, thiserror::Error)]
-#[allow(missing_docs)]
-pub enum TapeVerificationError {
-    #[error("incorrect tape length: expected {0}, got {1}")]
-    IncorrectLength(usize, usize),
-    #[error("incorrect share type")]
-    IncorrectShareType,
-    #[error("incorrect share value")]
-    IncorrectShareValue,
 }
