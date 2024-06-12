477448c01e93ce994b4013693f8a39cb20e4e76b
sinu <65924192+sinui0@users.noreply.github.com>
2024-06-12 11:17:50 -0700

    fix(mpz-common): flush io in syncer

--- crates/mpz-common/src/sync/async_syncer.rs
+++ crates/mpz-common/src/sync/async_syncer.rs
@@ -5,8 +5,8 @@ use std::{
     task::{ready, Context as StdContext, Poll, Waker},
 };
 
-use futures::{future::poll_fn, Future};
-use serio::{stream::IoStreamExt, IoDuplex};
+use futures::Future;
+use serio::{stream::IoStreamExt, IoDuplex, SinkExt};
 use tokio::sync::Mutex;
 
 use crate::sync::{SyncError, Ticket};
@@ -67,15 +67,11 @@ impl Leader {
     where
         Fut: Future,
     {
-        let mut io = Pin::new(io);
-        poll_fn(|cx| io.as_mut().poll_ready(cx)).await?;
-        let (output, tick) = {
-            let mut tick_lock = self.tick.lock().await;
-            let output = fut.await;
-            let tick = tick_lock.increment_in_place();
-            (output, tick)
-        };
-        io.start_send(tick)?;
+        let mut tick_lock = self.tick.lock().await;
+        io.send(tick_lock.increment_in_place()).await?;
+        let output = fut.await;
+        drop(tick_lock);
+
         Ok(output)
     }
 }
