9f72952cb476a24d7e362a69a09c8ab6fda44b36
th4s <th4s@metavoid.xyz>
2025-02-03 15:48:34 +0100

    doc: add comments

--- crates/lpn-estimator/src/lib.rs
+++ crates/lpn-estimator/src/lib.rs
@@ -2,12 +2,14 @@ mod lpn;
 
 pub use lpn::LpnEstimator;
 
+/// The primal LPN type.
 #[derive(Copy, Clone, Debug)]
 pub enum LpnType {
     Exact,
     Regular,
 }
 
+/// Parameters for an LPN instance.
 #[derive(Copy, Clone, Debug)]
 pub struct LpnParams {
     typ: LpnType,
@@ -18,6 +20,14 @@ pub struct LpnParams {
 }
 
 impl LpnParams {
+    /// Creates a primal LPN instance.
+    ///
+    /// # Arguments
+    ///
+    /// * `typ` - The LPN type.
+    /// * `n` - The number of samples.
+    /// * `k` - The length of the secret.
+    /// * `t` - The hamming weight of the noise.
     pub fn new(typ: LpnType, n: u64, k: u64, t: u64) -> Self {
         let s = match typ {
             LpnType::Exact => LpnEstimator::security_for_binary(n, k, t),
@@ -27,15 +37,27 @@ impl LpnParams {
         Self { typ, n, k, t, s }
     }
 
+    /// Scans the parameter space for LPN instances based on some fixed `k` and a minimum bit
+    /// security.
+    ///
+    /// Tries to maximize `n` while keeping `t` small and staying above a minimum provided bit
+    /// security.
+    ///
+    /// # Arguments
+    ///
+    /// * `typ` - The LPN type.
+    /// * `k` - The length of the secret.
+    /// * `s` - The minimum bit security.
+    /// * `max_n` - The maximum number of samples to consider.
     pub fn scan(typ: LpnType, s: f64, k: u64, max_n: Option<u64>) -> Vec<LpnParams> {
-        const MIN_K: u64 = 1024;
+        const MIN_K: u64 = 1000;
         assert!(k >= MIN_K, "k must be greter than {MIN_K}");
 
         const MAX_STEP: u32 = 4;
-        const MIN_T: u64 = 256;
+        const MIN_T: u64 = 200;
 
-        // Do not scan for n >~ 100_000_000
-        const MAX_N: u64 = 1 << 27;
+        // Do not scan for n > 100_000_000
+        const MAX_N: u64 = 100_000_000;
         let max_n = max_n.unwrap_or(MAX_N);
 
         let mut lpns = vec![];
@@ -75,14 +97,22 @@ impl LpnParams {
         lpns
     }
 
+    /// Returns the LPN type.
     pub fn typ(&self) -> LpnType {
         self.typ
     }
 
+    /// Returns the bit security.
     pub fn security(&self) -> f64 {
         self.s
     }
 
+    /// Returns (n, k, t).
+    ///
+    /// where
+    /// n - The number of samples.
+    /// k - The length of the secret.
+    /// t - The hamming weight of the noise.
     pub fn nkt(&self) -> (u64, u64, u64) {
         (self.n, self.k, self.t)
     }
