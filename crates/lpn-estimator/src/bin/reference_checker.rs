02426725d3c5cdd5501ac2aaaf156863240f0786
th4s <th4s@metavoid.xyz>
2025-02-12 16:35:43 +0100

    perf: added parallelization

--- crates/lpn-estimator/src/bin/reference_checker.rs
+++ crates/lpn-estimator/src/bin/reference_checker.rs
@@ -1,6 +1,10 @@
 use lpn_estimator::{LpnParams, LpnType};
-use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator};
-use std::{path::Path, process::Command};
+use std::{
+    path::Path,
+    process::Command,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 const ESTIMATOR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/lpnestimator.com/estimator.py");
 const EXPECTED_TABLE_FORMAT: &str = "t, n, s";
@@ -12,6 +16,10 @@ fn main() {
         panic!("You need to provide 3 parameters for the LPN reference checker.")
     }
 
+    let path = args.next().unwrap();
+    let path = Path::new(&path);
+    let table = std::fs::read_to_string(path).expect("unable to open table");
+
     let typ = match args.next().unwrap().as_str() {
         "exact" => LpnType::Exact,
         "regular" => LpnType::Regular,
@@ -20,28 +28,52 @@ fn main() {
 
     let k: u64 = args.next().unwrap().parse().expect("unable to parse k");
 
-    let path = args.next().unwrap();
-    let path = Path::new(&path);
-    let table = std::fs::read_to_string(path).expect("unable to open table");
-
     let lpns = create_lpns(table, typ, k);
-    let mut lpns_reference = Vec::with_capacity(lpns.len());
+    let reference_lpns: Arc<Mutex<Vec<Option<LpnParams>>>> =
+        Arc::new(Mutex::new(vec![None; lpns.len()]));
 
-    for lpn in lpns.iter() {
-        let lpn_ref = compute_security_with_reference(lpn);
+    for (k, &lpn) in lpns.iter().enumerate() {
+        let reference_lpns = reference_lpns.clone();
+
+        rayon::spawn(move || {
+            eprintln!("Spawning thread...");
+            let ref_lpn = compute_security_with_reference(lpn);
+            let mut reference_lpns = reference_lpns.lock().unwrap();
+            reference_lpns[k] = Some(ref_lpn);
+        });
+    }
+
+    loop {
+        std::thread::sleep(Duration::from_secs(1));
+        if let Ok(reference_lpns) = reference_lpns.lock() {
+            if reference_lpns.iter().all(|lpn| lpn.is_some()) {
+                break;
+            }
+        }
+    }
+
+    let reference_lpns = reference_lpns
+        .lock()
+        .unwrap()
+        .iter()
+        .copied()
+        .collect::<Option<Vec<_>>>()
+        .unwrap();
+
+    println!("\nComputed and reference bit securities:");
+    for (expected, reference) in lpns.iter().zip(reference_lpns.iter()) {
         println!(
-            "security: original {}, reference {}",
-            lpn.security(),
-            lpn_ref.security()
+            "{expected:?}, computed: {}, reference: {}",
+            expected.security(),
+            reference.security()
         );
-        lpns_reference.push(lpn_ref);
     }
 }
 
 fn create_lpns(table: String, typ: LpnType, k: u64) -> Vec<LpnParams> {
     let mut table = table.lines();
-
     let header = table.next().expect("unable to read table header");
+
     assert_eq!(
         header, EXPECTED_TABLE_FORMAT,
         "Expected a csv table for lpn parameters in the format ({})",
@@ -56,33 +88,36 @@ fn create_lpns(table: String, typ: LpnType, k: u64) -> Vec<LpnParams> {
         let t = values
             .next()
             .expect("unable to read t")
+            .trim()
             .parse::<u64>()
             .expect("unable to parse t");
 
         let n = values
             .next()
             .expect("unable to read n")
+            .trim()
             .parse::<u64>()
             .expect("unable to parse n");
 
         let s = values
             .next()
             .expect("unable to read s")
+            .trim()
             .parse::<u64>()
             .expect("unable to parse s");
 
         let lpn = LpnParams::new_with_security(typ, n, k, t, s as f64);
         lpns.push(lpn);
     }
+
     lpns
 }
 
-fn compute_security_with_reference(lpn: &LpnParams) -> LpnParams {
+fn compute_security_with_reference(lpn: LpnParams) -> LpnParams {
     let typ = match lpn.typ() {
         LpnType::Exact => "exact",
         LpnType::Regular => "regular",
-    }
-    .to_owned();
+    };
 
     let (n, k, t) = lpn.nkt();
 
@@ -95,7 +130,10 @@ fn compute_security_with_reference(lpn: &LpnParams) -> LpnParams {
         .output()
         .unwrap()
         .stdout;
-    let security = std::str::from_utf8(&security).expect("unable to read python output");
+
+    let security = std::str::from_utf8(&security)
+        .expect("unable to read python output")
+        .trim();
     let security: f64 = security.parse().expect("unable to parse security to f64");
 
     LpnParams::new_with_security(lpn.typ(), n, k, t, security)
