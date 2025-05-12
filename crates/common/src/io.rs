ccb4b87bc4d04769079af164ec7986d6d8629f1c
th4s <th4s@metavoid.xyz>
2025-05-12 13:52:52 +0200

    WIP: add `WithFrameLimit`...

--- crates/common/src/io.rs
+++ crates/common/src/io.rs
@@ -9,7 +9,10 @@ use bytes::{Bytes, BytesMut};
 use futures::{AsyncRead, AsyncWrite};
 use pin_project_lite::pin_project;
 use serio::{Framed, Sink, Stream, channel::MemoryDuplex, codec::Bincode};
-use tokio_util::{codec::LengthDelimitedCodec, compat::FuturesAsyncReadCompatExt as _};
+use tokio_util::{
+    codec::{Framed as TokioFramed, LengthDelimitedCodec},
+    compat::FuturesAsyncReadCompatExt as _,
+};
 
 trait Duplex:
     futures::Stream<Item = Result<BytesMut, std::io::Error>>
@@ -21,6 +24,83 @@ impl<T> Duplex for T where
     T: futures::Stream<Item = Result<BytesMut, std::io::Error>>
         + futures::Sink<Bytes, Error = std::io::Error>
 {
+}
+
+trait DuplexFrameLimited<T, U> {
+    /// Sets a new maximum frame length and returns a [`WithFrameLimit`].
+    ///
+    /// # Arguments
+    ///
+    /// * `max_frame_len` - The new maximum frame length in bytes.
+    fn with_max_frame_limit(&mut self, max_frame_len: usize) -> WithFrameLimit<'_, T>;
+}
+
+impl<T, U> DuplexFrameLimited<T, U> for Framed<TokioFramed<T, LengthDelimitedCodec>, U> {
+    fn with_max_frame_limit(&mut self, max_frame_len: usize) -> WithFrameLimit<'_, T> {
+        let old_frame_limit = self.inner().codec().max_frame_length();
+        self.inner_mut()
+            .codec_mut()
+            .set_max_frame_length(max_frame_len);
+
+        WithFrameLimit {
+            old_frame_limit,
+            framed: self.inner_mut(),
+        }
+    }
+}
+
+pin_project! {
+    /// Wrapper around [`Framed`] to temporarily set a new maximum frame length.
+    pub struct WithFrameLimit<'a, T> {
+        old_frame_limit: usize,
+        #[pin]
+        framed: &'a mut TokioFramed<T, LengthDelimitedCodec>,
+    }
+
+    impl<'a, T> PinnedDrop for WithFrameLimit<'a, T> {
+        fn drop(this: Pin<&mut Self>) {
+            let frame_limit = this.old_frame_limit;
+            this.project()
+                .framed
+                .codec_mut()
+                .set_max_frame_length(frame_limit);
+        }
+    }
+}
+
+impl<T> futures::Stream for WithFrameLimit<'_, T>
+where
+    TokioFramed<T, LengthDelimitedCodec>:
+        futures::Stream<Item = Result<BytesMut, std::io::Error>> + Unpin,
+{
+    type Item = Result<BytesMut, std::io::Error>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().framed.poll_next(cx)
+    }
+}
+
+impl<T> futures::Sink<Bytes> for WithFrameLimit<'_, T>
+where
+    TokioFramed<T, LengthDelimitedCodec>: futures::Sink<Bytes, Error = std::io::Error> + Unpin,
+{
+    type Error = std::io::Error;
+
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().framed.poll_ready(cx)
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: Bytes) -> Result<(), Self::Error> {
+        self.project().framed.start_send(item)
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().framed.poll_flush(cx)
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.project().framed.poll_close(cx)
+    }
 }
 
 pin_project! {
