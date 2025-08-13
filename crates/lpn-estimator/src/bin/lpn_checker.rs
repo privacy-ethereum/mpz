698892efcc5572ff629af3ad7898700f573c03b3
th4s <th4s@metavoid.xyz>
2025-08-13 19:00:44 +0200

    simplified lpn-checker

--- crates/lpn-estimator/src/bin/lpn_checker.rs
+++ crates/lpn-estimator/src/bin/lpn_checker.rs
@@ -1,83 +1,63 @@
 use lpn_estimator::{LpnParams, LpnType};
+use mpz_core::lpn::LpnParameters;
 use mpz_ot_core::ferret::{REGULAR_PARAMS, UNIFORM_PARAMS};
 use std::{
-    path::Path,
     process::Command,
     sync::{Arc, Mutex},
     time::Duration,
 };
 
-const ESTIMATOR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/lpnestimator.com/estimator.py");
-const MAX_SECS_WITHOUT_PROGRESS: u32 = 180;
+const REFERENCE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/lpnestimator.com/estimator.py");
 
 fn main() {
-    eprintln!(
-        "\nThis program checks the bit security of our LPN instances in ot-core/src/ferret/config
+    println!(
+        "\nThis program checks the bit security of our LPN instances in `ot-core/src/ferret/config` \
         against a reference implementation."
     );
+    println!();
 
-    let reference_lpns: Arc<Mutex<Vec<Option<LpnParams>>>> =
-        Arc::new(Mutex::new(vec![None; lpns.len()]));
+    check(UNIFORM_PARAMS.to_vec(), LpnType::Exact);
+    println!();
+    check(REGULAR_PARAMS.to_vec(), LpnType::Regular);
+}
+
+// Checks the lpn securities in parallel.
+fn check(lpns: Vec<LpnParameters>, typ: LpnType) {
+    let ref_securities: Arc<Mutex<Vec<Option<f64>>>> = Arc::new(Mutex::new(vec![None; lpns.len()]));
 
     for (k, &lpn) in lpns.iter().enumerate() {
-        let reference_lpns = reference_lpns.clone();
+        let ref_securities = ref_securities.clone();
 
         rayon::spawn(move || {
-            let ref_lpn = compute_security_with_reference_impl(lpn);
-            let mut reference_lpns = reference_lpns.lock().unwrap();
-            reference_lpns[k] = Some(ref_lpn);
-            eprintln!("Finished checking one table entry...");
+            let lpn = LpnParams::new(typ, lpn.n as u64, lpn.k as u64, lpn.t as u64);
+            let security = lpn.security();
+            let ref_security = compute_ref_security(&lpn);
+
+            let mut ref_securities = ref_securities.lock().unwrap();
+            ref_securities[k] = Some(ref_security);
+
+            let (n, k, t) = lpn.nkt();
+            println!(
+                "Checked {typ:?} LPN (n={}, k={}, t= {}): \tcomputed security: {:.2}, reference security: {:.2}",
+                n, k, t, security, ref_security
+            );
         });
     }
 
-    let mut count_finished = 0;
-    let mut seconds_elapsed_since_progress = 0;
     loop {
-        std::thread::sleep(Duration::from_secs(1));
-        seconds_elapsed_since_progress += 1;
-
-        if let Ok(reference_lpns) = reference_lpns.lock() {
-            // Put some maximum time on computation
-            let new_finished =
-                reference_lpns.iter().filter(|lpn| lpn.is_some()).count() - count_finished;
-            count_finished += new_finished;
-
-            if new_finished > 0 {
-                seconds_elapsed_since_progress = 0;
-            } else if new_finished == 0
-                && seconds_elapsed_since_progress >= MAX_SECS_WITHOUT_PROGRESS
-            {
-                eprintln!("Could not compute all instances! Computation taking too long.");
-                break;
-            }
-
-            // All finished
-            if reference_lpns.iter().all(|lpn| lpn.is_some()) {
+        {
+            let lpns = ref_securities.lock().unwrap();
+            if lpns.iter().all(|lpn| lpn.is_some()) {
                 break;
             }
         }
-    }
 
-    let reference_lpns = reference_lpns
-        .lock()
-        .unwrap()
-        .iter()
-        .filter(|lpn| lpn.is_some())
-        .copied()
-        .collect::<Option<Vec<_>>>()
-        .unwrap();
-
-    println!("\nShowing computed and reference bit securities:");
-    for (expected, reference) in lpns.iter().zip(reference_lpns.iter()) {
-        println!(
-            "{expected:?}, computed: {}, reference: {}",
-            expected.security() as u64,
-            reference.security() as u64
-        );
+        std::thread::sleep(Duration::from_secs(2));
     }
 }
 
-fn compute_security_with_reference_impl(lpn: LpnParams) -> LpnParams {
+// Computes the bit security using a reference implementation
+fn compute_ref_security(lpn: &LpnParams) -> f64 {
     let typ = match lpn.typ() {
         LpnType::Exact => "exact",
         LpnType::Regular => "regular",
@@ -86,7 +66,7 @@ fn compute_security_with_reference_impl(lpn: LpnParams) -> LpnParams {
     let (n, k, t) = lpn.nkt();
 
     let security = Command::new("python")
-        .arg(ESTIMATOR)
+        .arg(REFERENCE)
         .arg(format!("N={n}"))
         .arg(format!("k={k}"))
         .arg(format!("t={t}"))
@@ -98,7 +78,5 @@ fn compute_security_with_reference_impl(lpn: LpnParams) -> LpnParams {
     let security = std::str::from_utf8(&security)
         .expect("unable to read python output")
         .trim();
-    let security: f64 = security.parse().expect("unable to parse security to f64");
-
-    unsafe { LpnParams::new_with_security(lpn.typ(), n, k, t, security) }
+    security.parse().expect("unable to parse security to f64")
 }
