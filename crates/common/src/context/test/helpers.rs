d9305a4c191bf811abfce44bf9c5f521d3c774d6
sinu <65924192+sinui0@users.noreply.github.com>
2026-05-05 11:07:37 -0700

    revert test_st_context change

--- crates/common/src/context/test/helpers.rs
+++ crates/common/src/context/test/helpers.rs
@@ -1,16 +1,22 @@
 //! Basic test context helpers.
 
+use serio::channel::duplex;
+
 use crate::{
     context::Context,
     executor::{Executor, ExecutorBuilder},
+    io::Io,
     mux::test_framed_mux,
 };
 
-/// Creates a pair of single-threaded contexts using multiplexed I/O channels.
+/// Creates a pair of single-threaded contexts using memory I/O channels.
 pub fn test_st_context(io_buffer: usize) -> (Context, Context) {
-    let (mux_0, mux_1) = test_framed_mux(io_buffer);
+    let (io_0, io_1) = duplex(io_buffer);
 
-    (Context::new(mux_0).unwrap(), Context::new(mux_1).unwrap())
+    (
+        Context::from_io(Io::from_channel(io_0)),
+        Context::from_io(Io::from_channel(io_1)),
+    )
 }
 
 /// Creates a pair of multi-threaded executors sharing multiplexed I/O channels.
